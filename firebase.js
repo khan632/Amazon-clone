d1198a929d6178a2232bf06ed591d944b1d603b1
md areeb uddin <mdareeb.uddin@truelancer.com>
2022-12-03 03:10:58 +0530

    second commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBVHQgM6Q71ppBVKP_EOeuiyVVW1PB9fF0",
+    authDomain: "fir-287d5.firebaseapp.com",
+    projectId: "fir-287d5",
+    storageBucket: "fir-287d5.appspot.com",
+    messagingSenderId: "1066368032671",
+    appId: "1:1066368032671:web:72d63082c06f5dccd3a7aa"
+  };
